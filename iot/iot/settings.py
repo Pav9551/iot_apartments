5df7a24ad354fdba001da3c990c673c0c672951b
Pav9551 <root@vm6.template>
2022-12-24 22:56:57 +0300

    Первыйadded environment variables

--- iot/iot/settings.py
+++ iot/iot/settings.py
@@ -25,13 +25,15 @@ if path.exists(dotenv_path):
     print(".env: ok " + dotenv_path)
 else:
     print(".env: err " + dotenv_path)
+    print("getenv('SECRET_KEY')")
+    SECRET_KEY = getenv('SECRET_KEY')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = getenv('SECRET_KEY')
+# SECRET_KEY = getenv('SECRET_KEY')
 
-SECRET_KEY ='12345'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
