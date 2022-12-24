93279eab83a8647d4eccf96be3febfaaef1b9047
Pav9551 <root@vm6.template>
2022-12-24 20:20:15 +0300

    added fill_states

--- iot/iot/settings.py
+++ iot/iot/settings.py
@@ -29,7 +29,9 @@ else:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
+#SECRET_KEY = getenv('SECRET_KEY')
+
+SECRET_KEY ='12345'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
