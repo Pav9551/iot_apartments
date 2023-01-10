e5e6ee6e7532758725dcb003a497c198db1ed74b
Pav9551 <pav9551@yandex.ru>
2023-01-10 10:55:54 +0300

    added Chart.js

--- iot/iotapp/urls.py
+++ iot/iotapp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from iotapp import views
-
-
 app_name = 'iotapp'
 
 urlpatterns = [
     path('', views.line_chart, name='index'),
     path('chart', views.line_chart, name='line_chart'),
     path('chartJSON', views.line_chart_json, name='line_chart_json'),
+
 ]
