f559cbc7a69a73d423878eb83ac664f95a7b7062
Pav9551 <pav9551@yandex.ru>
2023-01-10 15:58:58 +0300

    added get_colors

--- iot/iotapp/views.py
+++ iot/iotapp/views.py
@@ -4,16 +4,22 @@ from django.shortcuts import render
 from random import randint
 from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
-
+from chartjs.colors import next_color
 
 class LineChartJSONView(BaseLineChartView):
     def get_labels(self):
         """Return 7 labels."""
         return ["January", "February", "March", "April", "May", "June", "July"]
 
+    def get_colors(self):
+        # 赤 / 緑 / 青　
+        #https://qtatsu.hatenablog.com/?page=1600594307
+        l = [(200, 0, 0), (0, 200, 0), (0, 0, 200)]
+        return next_color(l)
     def get_providers(self):
         """Return names of datasets."""
         return ["Central", "Eastside", "Westside"]
+
 
     def get_data(self):
         """Return 3 dataset to plot."""
@@ -25,6 +31,7 @@ class LineChartJSONView(BaseLineChartView):
 
 line_chart = TemplateView.as_view(template_name='iotapp/line_chart.html')#line_chart
 line_chart_json = LineChartJSONView.as_view()
+
 
 def main_view(request):
     return render(request, 'iotapp/index.html')
