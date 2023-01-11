aba4c686933f6ec7d3443e908bb2499bc0c8bdb1
Pav9551 <pav9551@yandex.ru>
2023-01-11 12:04:34 +0300

    added Chart1,Chart2 etc.

--- iot/iotapp/urls.py
+++ iot/iotapp/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('', views.line_chart, name='index'),
     path('chart', views.line_chart, name='line_chart'),
     path('chartJSON/<int:id>/', views.line_chart_json, name='line_chart_json'),
-    #path('MyView/<int:id>/', views.MyView.as_view(), name='My_View'),
-    #path('MyView/<int:id>/', views.MyView.as_view(), name='My_View'),
 ]
--- iot/iotapp/views.py
+++ iot/iotapp/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from random import randint
 from django.views import View
 from django.views.generic import TemplateView
-from chartjs.views.lines import BaseLineChartView
+from chartjs.views.lines import BaseLineChartView,BaseLineOptionsChartView
 from chartjs.colors import next_color
 class ChartMixin(object):
     def get_colors(self):
@@ -19,32 +19,31 @@ class ChartMixin(object):
 
     def get_providers(self):
         """Return names of datasets."""
-        return ["Central", "Eastside", "Westside"]
-class LineChartJSONView(ChartMixin,BaseLineChartView):
+        return ["temp", "sound", "Air"]
+class lineChartJSONView(ChartMixin, BaseLineOptionsChartView):
     def get_labels(self):
         """Return 7 labels."""
         return ["January", "February", "March", "April", "May", "June", "July"]
     def get_data(self):
         """Return 3 dataset to plot."""
+        print(self.kwargs['id'])
         r = randint(30, 60)
         return [[r, 44, 92, 11, 44, 95, 35],
                 [r, 92, 18, 3, 73, 87, 92],
                 [r, 21, 94, 3, 90, 13, 65]]
-class LineChartJSONView_Room1(ChartMixin,BaseLineChartView):
-    def get_labels(self):
-        """Return 7 labels."""
-        print(self.kwargs['id'])
-        X_labels = [1, 2, 3, 4, 5, 6, 7]
-        r = randint(30, 60)
-        self.Y_values = [[r, 44, 92, 11, 44, 95, 35],
-                        [r, 92, 18, 3, 73, 87, 92],
-                        [r, 21, 94, 3, 90, 13, 65]]
-        return X_labels
-    def get_data(self):
-        """Return 3 dataset to plot."""
-        return self.Y_values
+
+    def get_options(self):
+        options = {
+            "title": {"display": True, "text": f"Room{self.kwargs['id']}"},
+            "elements": {"point": {"pointStyle": "rectRounded", "radius": 4}},
+            "responsive": False,
+            "animation": False,
+            "roomid": self.kwargs['id'],
+        }
+        return options
 line_chart = TemplateView.as_view(template_name='iotapp/line_chart.html')#line_chart
-line_chart_json = LineChartJSONView_Room1.as_view()
+line_chart_json = lineChartJSONView.as_view()
+
 
 def main_view(request):
     return render(request, 'iotapp/index.html')
